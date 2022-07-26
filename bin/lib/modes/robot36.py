6ecc929fa6260da6c31fc13db118d20db98e2379
John Rudy <johnrudy@pop-os.localdomain>
2022-07-26 19:04:39 +0300

    modified sstv_utils and fixed encoders

--- bin/lib/modes/robot36.py
+++ bin/lib/modes/robot36.py
@@ -27,46 +27,77 @@ class Robot36(BaseEncoder):
         Returns a tuple (hertz, ms)
         """
         print("----- Beginning turning image to audio data -----")
+
         # header and vis
         self.hertz, self.duration = SSTV.initial()
         vis_code = SSTV.vis_code(settings.VISCode)
         v_hertz, v_dur = SSTV.vis_to_hertz(vis_code)
         self.add_to_data(v_hertz, v_dur)
+        
+        self.add_to_data([1300],[30]) # Parity
+        self.add_to_data([1200],[30]) # vis stop
+        
         for row in trange(239):
-            y_sl:list[float] = []
-            ry_sl:list[float] = []
-            by_sl:list[float] = []
-            # get the luminance and chrominance value for each pixel
+            y:list[float] = []
+            ry:list[float] = []
+            by:list[float] = []
+
             for column in range(319):
-                r = float(self.image[row,column][0])
-                g = float(self.image[row,column][1])
-                b = float(self.image[row,column][2])
-                l = SSTV.to_luminance(r, g, b)
-                c = SSTV.to_chrominance(r, g, b, l)
-                y_sl.append(l)
-                ry_sl.append(c[0])
-                by_sl.append(c[1])
+                yryby = SSTV.to_YRyBy(self.image[row,column])
+                y.append(yryby[0])
+                ry.append(yryby[1])
+                by.append(yryby[2])
+            
             # Sync    
-            self.add_to_data([settings.sync_hz['sync pulse']], [settings.sync_ms['sync pulse']])
+            self.add_to_data(
+                [settings.sync_hz['sync pulse']], 
+                [settings.sync_ms['sync pulse']]
+            )
             # Porch
-            self.add_to_data([settings.sync_hz['porch']], [settings.sync_ms['porch']]) 
-            # y line
-            y_hertz, y_dur = SSTV.scanline(y_sl, settings.y_pixel_ms )
-            self.add_to_data(y_hertz, y_dur)
-            # Even
+            self.add_to_data(
+                [settings.sync_hz['sync porch']], 
+                [settings.sync_ms['sync porch']]
+            ) 
+            
+            # Y values
+            for value in y:
+                self.hertz.append(SSTV.float_to_hertz(value))
+                self.duration.append(settings.y_pixel_ms)      
+
+            # Separator
             if row % 2 == 0:
-                # separator
-                self.add_to_data([settings.sync_hz['even separator']], [settings.sync_ms['even separator']])
-                # r-y
-                e_hertz, e_dur = SSTV.scanline(ry_sl, settings.rb_pixel_ms)
-                self.add_to_data(e_hertz,e_dur)
-            # odd
+                self.add_to_data(
+                    [settings.sync_hz['even separator']], 
+                    [settings.sync_ms['even separator']]
+                )
+                # Porch
+                self.add_to_data(
+                    [settings.sync_hz['porch']], 
+                    [settings.sync_ms['porch']]
+                ) 
+                
             if row % 2 == 1:
-                # separator
-                self.add_to_data([settings.sync_hz['odd separator']], [settings.sync_ms['odd separator']])
-                # b-y
-                o_hertz, o_dur = SSTV.scanline(by_sl, settings.rb_pixel_ms)
-                self.add_to_data(o_hertz, o_dur)
+                self.add_to_data(
+                    [settings.sync_hz['odd separator']], 
+                    [settings.sync_ms['odd separator']]
+                )
+                    # Porch
+                self.add_to_data(
+                    [settings.sync_hz['porch']], 
+                    [settings.sync_ms['porch']]
+                )
+                
+
+            # RyBy values 
+            for value in range(len(ry)):
+                if row % 2 == 0:
+                    self.hertz.append(SSTV.float_to_hertz(ry[value]))
+                    self.duration.append(settings.rb_pixel_ms)
+          
+                if row % 2 == 1:
+                    self.hertz.append(SSTV.float_to_hertz(by[value]))
+                    self.duration.append(settings.rb_pixel_ms)
+
         return self.hertz, self.duration
 
 if __name__ == "__main__":
--- bin/lib/modes/wraasesc2_120.py
+++ bin/lib/modes/wraasesc2_120.py
@@ -24,34 +24,42 @@ class WraaseSC2_120(BaseEncoder):
         v_hertz, v_dur = SSTV.vis_to_hertz(vis_code)
         self.hertz.extend(v_hertz)
         self.duration.extend(v_dur)
-        p_hd = SSTV.parity()
-        self.hertz.append(p_hd[0])
-        self.duration.append(p_hd[1])
+        self.hertz.append(1100)
+        self.duration.append(30)
+        self.hertz.append(1200)
+        self.duration.append(30)
+
+
         self.hertz.append(settings.sync_hz['sync pulse'])
         self.duration.append(settings.sync_ms['sync pulse'])
+        
         for row in trange(settings.image_size[1]):
-            red:list[float] = []
-            green:list[float] = []
-            blue:list[float] = []
+            y:list[float] = []
+            ry:list[float] = []
+            by:list[float] = []
+
             for column in range(settings.image_size[0]):
-                r = float(self.image[row,column][0])
-                g = float(self.image[row,column][1])
-                b = float(self.image[row,column][2])
-                l = SSTV.to_luminance(r, g, b)
-                c = SSTV.to_chrominance(r, g, b, l)
-                # These might be wrong, use the rgb values instead? 
-                red.append(c[0])
-                green.append(c[1])
-                blue.append(c[2])
-            r_h, r_d = SSTV.scanline(red, settings.redblue_pixel_ms)
-            g_h, g_d = SSTV.scanline(green,settings.green_pixel_ms)
-            b_h, b_d = SSTV.scanline(blue, settings.redblue_pixel_ms)
-            self.hertz.extend(r_h)
-            self.hertz.extend(g_h)
-            self.hertz.extend(b_h)
-            self.duration.extend(r_d)
-            self.duration.extend(g_d)
-            self.duration.extend(b_d)
+                YRyBy = SSTV.to_YRyBy(self.image[row,column])
+                y.append(YRyBy[0])
+                ry.append(YRyBy[1])
+                by.append(YRyBy[2])
+
+            # porch
+            self.hertz.append(1500)
+            self.duration.append(0.5)
+
+            for value in y:
+                self.hertz.append(SSTV.float_to_hertz(value))
+                self.duration.append(settings.redblue_pixel_ms)
+
+            for value in ry:
+                self.hertz.append(SSTV.float_to_hertz(value))
+                self.duration.append(settings.green_pixel_ms)
+
+            for value in by:
+                self.hertz.append(SSTV.float_to_hertz(value))
+                self.duration.append(settings.redblue_pixel_ms)
+
             self.hertz.append(settings.sync_hz['sync pulse'])
             self.duration.append(settings.sync_ms['sync pulse'])
         return self.hertz, self.duration
